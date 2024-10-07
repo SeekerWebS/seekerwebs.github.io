75c0edd3d98a112b85e921d28c75162052efc0c1
SeekerWebS <rhyanlus426@gmail.com>
2024-10-07 11:26:37 -0300

    Create Infinite-yield.lua

---
+++ scfiles/Infinite-yield.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SeekerWebS/seekerwebs.github.io/rbsSource/scs-game/dc-seeker.lua'))()
+--[[ 
+----------------------------------
+Nome: Infinite Yield
+descrição: Execute comandos admin.
+por; Rhyan57(896604349311115304) - ADMIN
+----------------------------------
+ID: 3
+SITE: https://seekerwebs.github.io/seekerSc.html
+SCRIPT SITE ORIGINAL: https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source
+----------------------------------
+]]--
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
